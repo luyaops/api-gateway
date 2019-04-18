eb51da618a76fdae4ff921175658d53a0a65c98e
yongpeng1 <yongpeng1@staff.sina.com.cn>
2019-04-18 14:29:01 +0800

    删除：相关命令行参数，改为同一使用fw中的命令行参数

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"flag"
 	"github.com/luyaops/api-gateway/loader"
 	"github.com/luyaops/api-gateway/server"
+	"github.com/luyaops/fw/common/config"
 	"github.com/luyaops/fw/common/log"
-	"os"
 )
-
-var hostBind, logLevel, etcdAddr string
 
 func main() {
 	log.Info("API-Gateway Start...")
-	loader.Services(etcdAddr)
-	server.Run(hostBind)
-}
-
-func init() {
-	var isHelp bool
-	flag.StringVar(&hostBind, "bind", ":8080", "Bind Address")
-	flag.StringVar(&etcdAddr, "etcdAddr", "localhost:2379", "Etcd Address")
-	flag.StringVar(&logLevel, "logLevel", "debug", "Log Level: debug info warn error fatal")
-	flag.BoolVar(&isHelp, "help", false, "Print this help")
-	flag.Parse()
-
-	log.SetLoggerLevel(logLevel)
-	if isHelp {
-		showHelp()
-	}
-}
-
-func showHelp() {
-	flag.PrintDefaults()
-	os.Exit(1)
+	loader.Services(config.Endpoints)
+	server.Run(config.GatewayBind)
 }
