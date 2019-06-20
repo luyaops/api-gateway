69ac532418f839a2827fe0bf6282ca46f9e1e391
yongpeng1 <yongpeng1@staff.sina.com.cn>
2019-06-20 18:41:21 +0800

    修复：只能注册单个服务问题

--- loader/load.go
+++ loader/load.go
@@ -2,6 +2,7 @@ package loader
 
 import (
 	"encoding/json"
+	_ "github.com/luyaops/cmdb/proto"
 	_ "github.com/luyaops/example/proto"
 	"github.com/luyaops/fw/common/constants"
 	"github.com/luyaops/fw/common/etcd"
@@ -27,14 +28,17 @@ func load(endpoints []string) {
 		log.Fatalf("Failed to load registration information:%v", err)
 	} else {
 		for _, v := range registration.Kvs {
-			err := json.Unmarshal(v.Value, &methods)
+			//fmt.Printf("%+v\n", string(v.Value))
+			var method []core.MethodWrapper
+			err := json.Unmarshal(v.Value, &method)
+			methods = append(methods, method...)
 			if err != nil {
 				log.Error(err)
 			}
 		}
 	}
+
 	for _, md := range methods {
-		//key := md.Pattern.Verb + ":" + md.Pattern.Path
 		key := md.Pattern.Verb + ":/" + API + md.Pattern.Path
 		log.Debug(key, " --> ", md.Service, ".", *md.Method.Name)
 		Store[key] = md
